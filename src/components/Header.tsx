37ed12ad73032d782a0a144445069a823c56674f
Frida Jonsson <annafridamariajonsson@gmail.com>
2025-12-04 10:30:11 +0100

    Styling: General layout & scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,19 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <div className={styles.headerBar}>
-      <div className={styles.appName}>
-      <h1> Prima</h1>
-      <h1>Lingua</h1>
+      <div className={styles.leftSection}>
+        <div className={styles.appName}>
+          <h1> Prima</h1>
+          <h1>Lingua</h1>
+        </div>
       </div>
-      <Navbar />
-      <LanguageBar />
+
+      <div className={styles.centerSection}>
+        <Navbar />
+      </div>
+      <div className={styles.rightSection}>
+        <LanguageBar />
+      </div>
     </div>
   )
 }
--- src/pages/Vocabulary.tsx
+++ src/pages/Vocabulary.tsx
@@ -10,44 +10,43 @@ function Vocabulary() {
 
   useEffect(() => {
     try {
-        const filteredVocabulary = vocabulary.filter(item => {
-          const categoryMatch = filterState.categories.length === 0 ||
-            filterState.categories.some(cat => cat.name === item.category.name);
-    
-          const classMatch = filterState.classes.length === 0 ||
-            filterState.classes.some(cls => cls.name === item.class.name);
-    
-          return categoryMatch && classMatch;
-        })
-        setFilteredVocab(filteredVocabulary);
-        if (filteredVocabulary.length === 0 && vocabulary.length > 0) {
-          throw new Error('No words found with the selected filters'); {/* TODO: Create error class, display error to user*/ }
-        }
+      const filteredVocabulary = vocabulary.filter(item => {
+        const categoryMatch = filterState.categories.length === 0 ||
+          filterState.categories.some(cat => cat.name === item.category.name);
+
+        const classMatch = filterState.classes.length === 0 ||
+          filterState.classes.some(cls => cls.name === item.class.name);
+
+        return categoryMatch && classMatch;
+      })
+      setFilteredVocab(filteredVocabulary);
+      if (filteredVocabulary.length === 0 && vocabulary.length > 0) {
+        throw new Error('No words found with the selected filters'); {/* TODO: Create error class, display error to user*/ }
+      }
     } catch (e) {
       console.error(e);
     }
   }, [filterState, vocabulary])
 
   return (
-    <div className={styles.vocabularyBox}>
-      <h2>Vocabulary</h2>
+    <div className={styles.vocabularyContainer}>
       {isLoadingVocabulary ? (
         <h2>Loading vocabulary...</h2>
       ) :
         error ? (<h3>Error: {error}</h3>) : (
           <>
             <FilterBar />
-            {/* TODO: WordGrid blocks other elements (customSelect) if empty */}
-            {/* Vocabulary acts as container & limit other elements - filterbar should be outside */}
-            {filteredVocab.length ? (
-              <div className={styles.wordGrid}>
-                {filteredVocab.map((word) => (
-                  <WordCard key={word._id} word={word} />
-                ))}
-              </div>
-            ) : (
-              <h3>No words found</h3>
-            )}
+            <div className={styles.vocabularyBox}>
+              {filteredVocab.length ? (
+                <div className={styles.wordGrid}>
+                  {filteredVocab.map((word) => (
+                    <WordCard key={word._id} word={word} />
+                  ))}
+                </div>
+              ) : (
+                <h3>No words found</h3>
+              )}
+            </div>
           </>
         )
       }
